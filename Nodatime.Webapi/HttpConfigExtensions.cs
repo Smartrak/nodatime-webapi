f5e995b108c600af0bb0ff3df8a30f1a49b8fc66
lloydstockman <ljstockman23@hotmail.com>
2016-06-01 11:28:09 +1200

    initial commit containing InstantModelBinder, which binds parameters to instants.

---
+++ Nodatime.Webapi/HttpConfigExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Http;
+using System.Web.Http.ModelBinding;
+using System.Web.Http.ModelBinding.Binders;
+using NodaTime;
+
+namespace Nodatime.Webapi
+{
+    public static class HttpConfigExtensions
+    {
+	    public static void ConfigureNodatimeWebapi(this HttpConfiguration config)
+	    {
+			var provider = new SimpleModelBinderProvider(typeof(Instant), new InstantModelBinder());
+			config.Services.Insert(typeof(ModelBinderProvider), 0, provider);
+		}
+    }
+}
---
+++ Nodatime.Webapi/InstantModelBinder.cs
@@ -0,0 +1,27 @@
+﻿using System.Web.Http.Controllers;
+using System.Web.Http.ModelBinding;
+using NodaTime.Text;
+
+namespace Nodatime.Webapi
+{
+	public class InstantModelBinder : IModelBinder
+	{
+		public bool BindModel(HttpActionContext actionContext, ModelBindingContext bindingContext)
+		{
+			var key = bindingContext.ModelName;
+			var val = bindingContext.ValueProvider.GetValue(key);
+			if (val != null)
+			{
+				var s = val.AttemptedValue;
+
+				var result = InstantPattern.ExtendedIsoPattern.Parse(s);
+				if (result.Success)
+				{
+					bindingContext.Model = result.Value;
+					return true;
+				}
+			}
+			return false;
+		}
+	}
+}
